87693178d7c69ea0bd53a6569c4fcdfef4e14ec2
ehsanshirin <47168571+ehsanshirin@users.noreply.github.com>
2025-11-02 20:08:26 +0330

    Rename

---
+++ Largest Number in a list.py
@@ -0,0 +1,9 @@
+# The largest number in a list.
+
+nums = list (map (int ,input ('type multiple number, with space: ').split()))
+nums.sort()
+print(nums[-1])
+
+
+# 213312
+# 4E5K481C6PJ4ZDLTEZMCDKG2
