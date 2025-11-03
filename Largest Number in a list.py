c095586f07f9278de6c5ce5f2181e817d3fad8e1
ehsanshirin <>
2025-11-03 15:52:59 +0330

    Update

--- Largest Number in a list.py
+++ Largest Number in a list.py
@@ -3,3 +3,9 @@
 nums = list (map (int ,input ('type multiple number, with space: ').split()))
 nums.sort()
 print(nums[-1])
+
+
+# The largest number in a list with max.
+
+nums = list(map(int, input('Type multiple numbers, separated by spaces: ').split()))
+print(f'The largest number is {max(nums)}')
