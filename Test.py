7566c70954f5f9be9508cd0e8d94b5b17b7a0d5a
ehsanshirin <47168571+ehsanshirin@users.noreply.github.com>
2025-11-06 16:54:31 +0330

    Create

--- Test.py
+++ Test.py
@@ -1,6 +1,36 @@
-x = int (input('type a number: '))
+# 2.
 
-if x % 2 == 0:
-    print(f'{x} is even')
-else:
-    print(f'{x} is odd')  
+# x = int (input('type a number: ')) 
+# y = int (input('type secound number: '))
+# print(f'''
+#       {x} + {y} = {x + y}\n
+#       {x} - {y} = {x - y}\n
+#       {x} * {y} = {x * y}\n
+#       {x} \ {y} = {x / y}
+#       ''')  
+
+# 3.
+# x = int (input('type a number: '))
+
+# if x % 2 == 0:
+#     print(f'{x} is even')
+# else:
+#     print(f'{x} is odd')  
+
+# 4.
+# n = int ( input ('write your num: '))
+# count = 0
+
+# while count < n:
+#     count += 1
+#     pass
+    
+#     print(count)
+
+
+nums = [1 , 2, 3, 4 , 5,10]
+result = 0
+for i in nums:
+    result += i
+    
+print(result)
