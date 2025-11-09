83738e0f97835e52aa03d79a847545d579b80f9b
ehsanshirin <>
2025-11-09 12:10:07 +0330

    Update

--- Calculating the (sqr & cube) root of a num.py
+++ Calculating the (sqr & cube) root of a num.py
@@ -1,6 +1,9 @@
 #Calculating the square root and cube root of a number.
 
+import math
 num = int (input ('Write a num: '))
-sqr = round(num ** (1/2))
-cube = round(num ** (1/3))
-print(sqr , cube)
+
+sqr = round(math.sqrt(num),2)
+cube = round(num ** (1/3), 2)
+
+print(f'Squre {num} = {sqr}\nCube {num} = {cube}')
