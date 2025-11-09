c061f3d346274769b29260b3bb80202e0649f3c9
ehsanshirin <>
2025-11-09 11:53:26 +0330

    Complate

---
+++ Calculating the (sqr & cube) root of a num.py
@@ -0,0 +1,6 @@
+#Calculating the square root and cube root of a number.
+
+num = int (input ('Write a num: '))
+sqr = round(num ** (1/2))
+cube = round(num ** (1/3))
+print(sqr , cube)
--- Test.py
+++
@@ -1,4 +0,0 @@
-def my_func(n):
-    return lambda a : a * n
-
-print(my_func(3)(5))
