0a20a78dd92f611416c7247e5b26392ade6e8112
ehsanshirin <>
2025-11-20 14:12:33 +0330

    Create

---
+++ Notebook.py
@@ -0,0 +1,28 @@
+﻿def add_note():
+    note = input('add your note: ')
+    with open('notebook.txt', 'a') as f:
+        f.write(note + '\n')
+        
+def view_note():
+    with open('notebook.txt' , 'r') as f:
+        notes = f.readlines()
+    for i, note in enumerate(notes, 1):
+        # با شماره‌گذاری از 1 به بالا، یادداشت‌ها را چاپ می‌کند.
+        print(f'{i}. {note}', end='')
+        # هر یادداشت را با شماره آن چاپ می‌کند.
+        # باعث می‌شود که دستور پرینت خودش خط جدید اضافه نکند ،end=''
+        # چون یادداشت‌ها از قبل با"اسلش ان" پایان یافته‌اند
+        
+while True:
+    print('\n1: add new note\n2: View Nots\n3: Exit')
+    
+    choise = input('Your Choise: ')
+    
+    if choise == '1':
+        add_note()
+    elif choise == '2':
+        view_note()
+    elif choise == '3':
+        break
+    else:
+        print('Error')
--- Test.py
+++ Test.py
@@ -1,10 +1,2 @@
-try:
-    f = open('demofile.txt')
-try:
-    f.write('LorumIpsum')
-except:
-    print('something went wrong when')
-finally:
-    f.close()
-except:
-    print('ok')
+with open('demofile.txt') as f:
+    print(f.read())
