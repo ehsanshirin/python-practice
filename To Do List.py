4a477e1b08c7190cf9ea02588f04304979761913
ehsanshirin <47168571+ehsanshirin@users.noreply.github.com>
2025-12-13 19:35:04 +0330

    update

--- To Do List.py
+++ To Do List.py
@@ -1,1 +1,25 @@
-﻿
+﻿class ToDoList:
+    def __init__(self):
+        self.todolist = {}
+        
+    def add(self, title):
+        self.todolist[title] = input('type describtion: ')
+        
+        print(f'{title} add to list')
+        
+    
+    def remove(self, title):
+        
+        print(f'{title} remove from list')
+
+    
+    def show(self):
+        print('Your List: ')
+        for i in self.todolist:
+            print(i)
+            
+            
+myList = ToDoList()
+myList.add('Python')
+
+myList.show()
