630c1ea95b3815a3f69801a42bfed43a7fe2c226
ehsanshirin <47168571+ehsanshirin@users.noreply.github.com>
2025-12-14 19:06:19 +0330

    complate

--- To Do List.py
+++ To Do List.py
@@ -3,24 +3,39 @@
         self.todolist = {}
         
     def add(self):
-        self.title = input('type describtion: ')
-        self.describt = input('Type yore describt: ')
+        title = input('Title: ')
+        if title in self.todolist:
+            print('There is a job posted.')
+            return
+        
+        self.title = title
+        self.describt = input('Describt: ')
         self.todolist[self.title] = self.describt
         print(f'{self.title} add to list')
         
     
-    def remove(self, title):
+    def remove(self):
+        self.title = input('Type Your Title: ')
+        if self.title not in self.todolist.keys():
+            print(f'{self.title} not find.')
+            return
+        self.todolist.pop(self.title)
+        print(f'{self.title} remove from list')
         
-        print(f'{title} remove from list')
+        
+    def done(self):
+        pass
 
     
     def show(self):
-        print(self.todolist)
-        # for i in self.todolist:
-        #     print(i)
+        n = 0
+        for key, value in self.todolist.items():
+            n += 1
+            print(f'{n}.{key}: {value}')
             
             
 myList = ToDoList()
 myList.add()
-
+myList.add()
+myList.remove()
 myList.show()
