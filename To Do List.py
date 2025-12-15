f69bbcf76c7f312367641f518b5c0608249775c5
ehsanshirin <ehsanshirin70@gmai.com>
2025-12-15 12:21:32 +0330

    update

--- To Do List.py
+++ To Do List.py
@@ -14,11 +14,20 @@
         print(f'{title} add to list')
         
     
-    def remove(self):
-        title = input('Type Your Title: ')
-        if title not in self.todolist.keys():
+    def remove(self):     
+        n = 0
+        for key, value in self.todolist.items():
+            n += 1
+            todolist = list((f'{n}.{key}: {value}'))
+            todolist.append(todolist)
+            print(todolist)
+
+        title = int(input('type num from list: '))
+        
+        if n not in self.todolist.keys():
             print(f'{title} not find.')
             return
+        
         self.todolist.pop(title)
         print(f'{title} remove from list')
         
