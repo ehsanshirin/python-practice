957b32a171e4f5d1f7eac027cd5d7c1caaea307b
ehsanshirin <ehsanshirin70@gmai.com>
2025-12-26 18:37:34 +0330

    upadate

--- Practise.py
+++ Practise.py
@@ -1,21 +1,39 @@
 class Shop:
-    def __init__(self, product_name, stock):
+    def __init__(self):
         pass
 
-    def add_product(self, product_name, stock):
+    def add_product(self, product_name, stock, price):
         pass
 
-    def reduce_srock(self, product_name, amount):
+    def reduce_stock(self, product_name, amount):
         pass
 
-    def show
+    def show_list(self):
+        pass
         
 
 
 
 class Custumer:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.orders = []
+
+    def add_order(self, order):
+        self.orders.append(order)
+
+    def show_order(self):
+        print(f'Orders fo {self.name}:')
+        for order in self.orders:
+            print(order)
 
 
 class Order:
-    pass
+    def __init__(self, custumer, product_name, amount):
+        self.custumer = custumer
+        self.pruduct_name = product_name
+        self.amount = amount
+        
+    
+    def __str__(self):
+        return f'{self.custumer.name} order {self.amount} of {self.pruduct_name}'
