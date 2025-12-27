47e585bc8d892e1a578df76eccb0211f3ff3dabb
ehsanshirin <ehsanshirin70@gmai.com>
2025-12-27 12:28:28 +0330

    update

--- Practise.py
+++ Practise.py
@@ -1,39 +1,11 @@
-class Shop:
-    def __init__(self):
-        pass
+class Car:
+    def __init__(self, year, brand):
+        self.year = year
+        self.brand = brand
 
-    def add_product(self, product_name, stock, price):
-        pass
-
-    def reduce_stock(self, product_name, amount):
-        pass
-
-    def show_list(self):
-        pass
-        
+    def show(self):
+        print(f'Brand: {self.brand}, Year: {self.year}')
 
 
-
-class Custumer:
-    def __init__(self, name):
-        self.name = name
-        self.orders = []
-
-    def add_order(self, order):
-        self.orders.append(order)
-
-    def show_order(self):
-        print(f'Orders fo {self.name}:')
-        for order in self.orders:
-            print(order)
-
-
-class Order:
-    def __init__(self, custumer, product_name, amount):
-        self.custumer = custumer
-        self.pruduct_name = product_name
-        self.amount = amount
-        
-    
-    def __str__(self):
-        return f'{self.custumer.name} order {self.amount} of {self.pruduct_name}'
+x = Car(1442, 'Benz')
+x.show()
