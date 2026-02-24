2d36f931edd7b0d0fdb1d76114376a6ac2577456
ehsanshirin <47168571+ehsanshirin@users.noreply.github.com>
2026-02-24 20:14:59 +0330

    new

---
+++ Practise_01.py
@@ -0,0 +1,9 @@
+# names = ["Mina", "Kian", "Nazi", "Arash"]
+
+# for i , lis in enumerate(names, start=1):
+#     print(f'{i} - {lis}')
+
+import tools
+
+lis = [10,12,34,25,15]
+print(tools.avrage(lis))
---
+++ tools.py
@@ -0,0 +1,4 @@
+def avrage(num_lis):
+    if not num_lis:
+        return 0
+    return sum(num_lis)/len(num_lis)
