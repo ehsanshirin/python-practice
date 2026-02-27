5549229c3b33c8a29cd1674cf333f03a8e3c63fb
ehsanshirin <ehsanshirin70@gmai.com>
2026-02-27 16:27:02 +0330

    update

--- Practise_01.py
+++ Practise_01.py
@@ -3,7 +3,10 @@
 # for i , lis in enumerate(names, start=1):
 #     print(f'{i} - {lis}')
 
-import tools
+# import tools
 
-lis = [10,12,34,25,15]
-print(tools.avrage(lis))
+# lis = [10,12,34,25,15]
+# print(tools.avrage(lis))
+
+for i , indax in enumerate(range (1, 5),start=1):
+    print(i*indax)
--- tools.py
+++ tools.py
@@ -1,4 +1,24 @@
-def avrage(num_lis):
-    if not num_lis:
-        return 0
-    return sum(num_lis)/len(num_lis)
+# math_tools.py
+
+# متغیر سطح ماژول
+PI = 3.14159
+
+def add(a, b):
+    """جمع دو عدد"""
+    return a + b
+
+
+def multiply(a, b):
+    """ضرب دو عدد"""
+    return a * b
+
+
+def circle_area(radius):
+    """مساحت دایره"""
+    return PI * radius ** 2
+
+
+# این قسمت فقط وقتی فایل مستقیم اجرا شود اجرا می‌شود (نه وقتی import شود)
+if __name__ == "__main__":
+    print("این فایل به صورت مستقیم اجرا شد!")
+    print("تست تابع:", add(10, 25))
