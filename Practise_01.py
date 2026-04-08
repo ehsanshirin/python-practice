7643b706af6857ef4adece4a18f402830a598f34
ehsanshirin <47168571+ehsanshirin@users.noreply.github.com>
2026-04-08 21:22:55 +0330

    update

--- Practise_01.py
+++ Practise_01.py
@@ -1,2 +1,11 @@
-cart = ['Mobile', 'Laptop', 'Monitor']
+def add_number(a, b):
+    return a + b
 
+def multiple_number(a, b):
+    return a * b
+
+def calculate_avrage():
+    number = input('type nums: ').split(sep=',')
+    print(number)
+    
+calculate_avrage()
